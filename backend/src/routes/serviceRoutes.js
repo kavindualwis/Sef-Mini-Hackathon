ca6986d23283066c808331fe6537d75e3f381726
Kavindualwis <kavindualwis02@proton.me>
2026-09-04 12:31:22 +0530

    Updated Service Route

--- backend/src/routes/serviceRoutes.js
+++ backend/src/routes/serviceRoutes.js
@@ -4,12 +4,14 @@ const {
   createService,
   getAllServices,
   getMyServices,
+  updateService,
   deleteService,
 } = require('../controllers/serviceController');
 
 router.post('/', createService);
 router.get('/', getAllServices);
 router.get('/my-services', getMyServices);
+router.put('/:id', updateService);
 router.delete('/:id', deleteService);
 
 module.exports = router;
